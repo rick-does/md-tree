36778a8cf82ece9670ed44042a9ae22f8b30293e
Rick Mallery <richard_mallery@yahoo.com>
2026-04-03 18:12:34 -0600

    removing screenshot files

---
+++ projects/_archive/test-5/sidebars.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "docs": [
+    {
+      "type": "category",
+      "label": "1",
+      "link": {
+        "type": "doc",
+        "id": "1"
+      },
+      "items": [
+        {
+          "type": "doc",
+          "id": "2",
+          "label": "2"
+        }
+      ]
+    }
+  ]
+};
